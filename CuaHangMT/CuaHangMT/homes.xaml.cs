7cf49c32ead55074cc5c0ae105ccf2ec0a520c97
BuiViet713 <21012528@st.phenikaa-uni.edu.vn>
2023-11-07 10:43:07 +0700

    lan1

---
+++ CuaHangMT/CuaHangMT/homes.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CuaHangMT.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CuaHangMT
+{
+	/// <summary>
+	/// Interaction logic for homes.xaml
+	/// </summary>
+	public partial class homes : Window
+	{
+		public homes()
+		{
+			InitializeComponent();
+		}
+	}
+}
